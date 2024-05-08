d8d11f3b116a36b955b60121497512a01674a57f
Roberto Rondineli Fernandes de Lima <betofl45v.8@gmail.com>
2024-05-08 18:56:03 -0300

    Update rascunhos.py

--- Materiais/rascunhos.py
+++ Materiais/rascunhos.py
@@ -1,3 +1,14 @@
-nome = input("Entre com seu nome: \n")
-for letra in nome:
-    print(letra)
+def func1(x):
+    x = 10
+    print(f'Função func1 - x = {x}')
+
+
+def func2(x):
+    x = 20
+    print(f'Função func2 - x = {x}')
+
+
+x = 0
+func1(x)
+func2(x)
+print(f'Programa principal - x = {x}')
