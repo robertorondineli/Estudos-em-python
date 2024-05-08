2ea714b212b17d48cdee463d0e068bccc0f3e1b6
Roberto Rondineli Fernandes de Lima <betofl45v.8@gmail.com>
2024-05-08 11:37:43 -0300

    Update rascunhos.py

--- Materiais/rascunhos.py
+++ Materiais/rascunhos.py
@@ -1,4 +1,3 @@
-peso = float(input("Digite o seu peso: "))
-altura = float(input("Digite a sua altura: "))
-imc = peso / (altura**2)
-print(f"o seu IMC é: {imc}")
+nome = input("Entre com seu nome: \n")
+for letra in nome:
+    print(letra)
