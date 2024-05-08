8650ffca4bf21c2856d9ee91c28b68f9442c70f1
Roberto Rondineli Fernandes de Lima <betofl45v.8@gmail.com>
2024-05-08 12:23:22 -0300

    Create soma_dos_pares.py

---
+++ Materiais/soma_dos_pares.py
@@ -0,0 +1,11 @@
+# Uma solução que retorne a soma de todos os elementos pares de uma lista
+def soma_par(lista):
+    global par
+    par = 0
+    for numero in lista:
+        if (numero % 2) == 0:
+            par += numero
+    print(f"A soma dos números pares da sua lista é: {par}")
+
+lista = [10, 2, 5, 7, 3]
+soma_par(lista)
